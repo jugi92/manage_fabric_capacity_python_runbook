28052ddede5f022cbcb045f5f8c0b825c1bf6ee3
julian.gimbel@microsoft.com <julian.gimbel@microsoft.com>
2024-02-05 19:05:30 +0100

    Add command line arguments for resource ID, operation, and SKU

--- suspend_or_resume_fabric_capacity.py
+++ suspend_or_resume_fabric_capacity.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
-# use this like `python3 suspend_or_resume_fabric_capacity.py /subscriptions/12345678-1234-1234-1234-123a12b12d1c/resourceGroups/fabric-rg/providers/Microsoft.Fabric/capacities/myf2capacity suspend`
-import sys
+# use this to suspend like `python3 suspend_or_resume_fabric_capacity.py /subscriptions/12345678-1234-1234-1234-123a12b12d1c/resourceGroups/fabric-rg/providers/Microsoft.Fabric/capacities/myf2capacity suspend`
+# use this to resume like `python3 suspend_or_resume_fabric_capacity.py /subscriptions/12345678-1234-1234-1234-123a12b12d1c/resourceGroups/fabric-rg/providers/Microsoft.Fabric/capacities/myf2capacity resume`
+# use this to scale like `python3 suspend_or_resume_fabric_capacity.py /subscriptions/12345678-1234-1234-1234-123a12b12d1c/resourceGroups/fabric-rg/providers/Microsoft.Fabric/capacities/myf2capacity scale F4`
+import argparse
 import os
 
 import requests
 
+parser = argparse.ArgumentParser()
+parser.add_argument("resource_id", help="The resource id of the capacity to change, e.g. /subscriptions/12345678-1234-1234-1234-123a12b12d1c/resourceGroups/fabric-rg/providers/Microsoft.Fabric/capacities/myf2capacity")
+parser.add_argument("operation", help="The operation to perform, either suspend, resume or scale")
+parser.add_argument("sku", nargs="?", help="The sku to scale to, e.g. F4")
+args = parser.parse_args()
 
-# resource_id = "/subscriptions/12345678-1234-1234-1234-123a12b12d1c/resourceGroups/fabric-rg/providers/Microsoft.Fabric/capacities/myf2capacity"
-resource_id = str(sys.argv[1])
+resource_id = args.resource_id
 
-# resume_or_suspend = "suspend"
-resume_or_suspend = str(sys.argv[2])
 operation = "suspend"
-if resume_or_suspend == "resume":
+if args.operation == "resume":
     operation = "resume"
+if args.operation == "scale":
+    operation = "scale"
+    
 
 if os.getenv('IDENTITY_ENDPOINT'):
     # using managed identity
@@ -32,10 +39,21 @@ else:
     credential = DefaultAzureCredential()
     token = credential.get_token("https://management.azure.com/").token
 
-url = f"https://management.azure.com{resource_id}/{operation}?api-version=2022-07-01-preview"
-print(f"INFO: Calling {url}")
-response = requests.post(url, headers={'Content-Type': 'application/json', "Authorization": f"Bearer {token}"})
-if not response.ok and response.json()["error"]["message"] == 'Service is not ready to be updated':
-    print(f"WARN: Service is not ready to be updated, probably it is already in desired state: {resume_or_suspend}")
-else:
+baseurl = f"https://management.azure.com{resource_id}/"
+
+if operation == "scale":
+    sku = args.sku
+    url = f"https://management.azure.com{resource_id}?api-version=2022-07-01-preview"
+    print(f"INFO: Scaling {url} to {sku}")
+    payload = {"sku":{"name": sku,"tier":"Fabric"}}
+    response = requests.patch(url, headers={'Content-Type': 'application/json', "Authorization": f"Bearer {token}"}, json=payload)
     response.raise_for_status()
+
+else:
+    url = f"https://management.azure.com{resource_id}/{operation}?api-version=2022-07-01-preview"
+    print(f"INFO: Calling {url}")
+    response = requests.post(url, headers={'Content-Type': 'application/json', "Authorization": f"Bearer {token}"})
+    if not response.ok and response.json()["error"]["message"] == 'Service is not ready to be updated':
+        print(f"WARN: Service is not ready to be updated, probably it is already in desired state: {operation}")
+    else:
+        response.raise_for_status()
