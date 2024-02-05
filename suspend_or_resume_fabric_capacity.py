05fbf9732b038c06533c812a193c2a4a4585a5dd
julian.gimbel@microsoft.com <julian.gimbel@microsoft.com>
2024-02-05 18:09:06 +0100

    proper error handling

--- suspend_or_resume_fabric_capacity.py
+++ suspend_or_resume_fabric_capacity.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # use this like `python3 suspend_or_resume_fabric_capacity.py /subscriptions/12345678-1234-1234-1234-123a12b12d1c/resourceGroups/fabric-rg/providers/Microsoft.Fabric/capacities/myf2capacity suspend`
+import logging
 import sys
 import os
 
@@ -25,6 +26,7 @@ if os.getenv('IDENTITY_ENDPOINT'):
     'Metadata': 'True' 
     } 
     response = requests.request("GET", endPoint, headers=headers, data=payload) 
+    response.raise_for_status()
     token = response.json()["access_token"]
 else:
     from azure.identity import DefaultAzureCredential
@@ -32,6 +34,9 @@ else:
     token = credential.get_token("https://management.azure.com/").token
 
 url = f"https://management.azure.com{resource_id}/{operation}?api-version=2022-07-01-preview"
-print(url)
-res = requests.post(url, headers={'Content-Type': 'application/json', "Authorization": f"Bearer {token}"})
-res.raise_for_status()
+logging.info(f"Calling {url}")
+response = requests.post(url, headers={'Content-Type': 'application/json', "Authorization": f"Bearer {token}"})
+if not response.ok and response.json()["error"]["message"] == 'Service is not ready to be updated':
+    logging.warning(f"Service is not ready to be updated, probably it is already in desired state: {resume_or_suspend}")
+else:
+    response.raise_for_status()
