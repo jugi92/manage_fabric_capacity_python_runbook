d548bb31c1ddaf3a6caef775a17513aeb3961df4
julian.gimbel@microsoft.com <julian.gimbel@microsoft.com>
2024-02-05 18:20:43 +0100

    logging does not work for runbook, reverting to print statements

--- suspend_or_resume_fabric_capacity.py
+++ suspend_or_resume_fabric_capacity.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # use this like `python3 suspend_or_resume_fabric_capacity.py /subscriptions/12345678-1234-1234-1234-123a12b12d1c/resourceGroups/fabric-rg/providers/Microsoft.Fabric/capacities/myf2capacity suspend`
-import logging
 import sys
 import os
 
@@ -34,9 +33,9 @@ else:
     token = credential.get_token("https://management.azure.com/").token
 
 url = f"https://management.azure.com{resource_id}/{operation}?api-version=2022-07-01-preview"
-logging.info(f"Calling {url}")
+print(f"INFO: Calling {url}")
 response = requests.post(url, headers={'Content-Type': 'application/json', "Authorization": f"Bearer {token}"})
 if not response.ok and response.json()["error"]["message"] == 'Service is not ready to be updated':
-    logging.warning(f"Service is not ready to be updated, probably it is already in desired state: {resume_or_suspend}")
+    print(f"WARN: Service is not ready to be updated, probably it is already in desired state: {resume_or_suspend}")
 else:
     response.raise_for_status()
